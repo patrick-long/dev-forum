1eae26121ab3a2c2996ca087148f8fea19398a8b
longpatrick <long.patrick1997@gmail.com>
2021-03-24 14:37:36 -0500

    add ability to cancel editing through anchor in edit.ejs; content while editing response now required

--- models/forum.js
+++ models/forum.js
@@ -2,13 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const responseSchema = new Schema ({
-    content: String,
+    content: {
+        type: String,
+        required: true
+    },
     rating: {
         type: Number,
         min: 1,
         max: 5,
         default: 5
-    }
+    },
+
 },  {
     timestamps: true
 })
